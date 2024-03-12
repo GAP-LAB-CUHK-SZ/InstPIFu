08ac313ade86552f1b86ccfd581b3698ba650f0b
UncleMEDM <115010192@link.cuhk.edu.cn>
2024-03-12 16:10:58 +0800

    update real demo

--- real_demo/run_demo.sh
+++ real_demo/run_demo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-instpifu_dir=/apdcephfs/private_haolinliu/InstPIFu
-yolo_dir=/apdcephfs/private_haolinliu/yolov7 #need to firstly install prerequired package for both yolov7 and im3d
-im3d_dir=/apdcephfs/private_haolinliu/Implicit3DUnderstanding
-sunrgbd_dir=/apdcephfs_cq3/share_1330077/haolinliu/data/sunrgbd_train_test_data
+instpifu_dir=/path/to/instpifu
+yolo_dir=/path/to/yolov7 #need to firstly install prerequired package for both yolov7 and im3d
+im3d_dir=/path/to/Implicit3DUnderstanding
+sunrgbd_dir=/path/to/sunrgbd_train_test_data
 taskid="2004" #taskid from sunrgbd dataset
 
 python 1.run_2d_detection.py --yolo_dir $yolo_dir --instpifu_dir $instpifu_dir --sunrgbd_dir $sunrgbd_dir --taskid $taskid
